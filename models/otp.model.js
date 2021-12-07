2bf3c1236e1b0faed109b713485502c12423f703
Stanislas-Web <stanislasmakengo1@gmail.com>
2021-12-07 16:29:47 +0100

    update time otp for 10 minutes

--- models/otp.model.js
+++ models/otp.model.js
@@ -4,5 +4,5 @@ module.exports.Otp = model('Otp', new Schema({
     number: { type: String, required: true },
     otp: { type: String, required: true },
     name: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now, expires: 1000 }
+    createdAt: { type: Date, default: Date.now, expires: 10000 }
 }, {timestamps: true }));
